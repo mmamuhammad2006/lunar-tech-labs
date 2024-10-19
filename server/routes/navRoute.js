5764129cea7ef137a1cb077650d8ccef06afb7ea
mmamuhammad2006 <168419071+mmamuhammad2006@users.noreply.github.com>
2024-10-19 16:37:40 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ server/routes/navRoute.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const cacheExpress = require('cache-express');
+const router = express.Router();
+
+const cache = cacheExpress({
+    duration: 3600000,
+    cacheControl: true,
+    maxAge: 3600
+});
+router.use(cache);
+router.get('/',(req,res)=>{
+    try{
+        res.render('pages/home',{title: 'Home',target: '#services-sec'});
+    }catch(error){
+        res.status(500).send('Internal Server Error')
+    }
+});
+router.get('/services',(req,res)=>{
+    try{
+        res.render('pages/services',{title: 'Products',target: '#products-sec'});
+    }catch(error){
+        res.status(500).send('Internal Server Error')
+    }
+});
+router.get('/contact-us',(req,res)=>{
+    try{
+        res.render('pages/contactUS',{title: 'Contact Us',target: '#contactUs-sec'});
+    }catch (error){
+        res.status(500).send('Internal Server Error')
+    }
+});
+router.get('/about-us',(req,res)=>{
+    try{
+        res.render('pages/aboutUs',{title: 'About Us',target: '#aboutUs-sec'});
+    }catch (error){
+        res.status(500).send('Internal Server Error')
+    }
+});
+module.exports = router;
